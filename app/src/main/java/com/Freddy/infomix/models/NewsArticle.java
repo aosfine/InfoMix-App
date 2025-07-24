9eb0a58523475ebe7ecd33cc654b880a7764897a
aosfine <64331967+aosfine@users.noreply.github.com>
2025-07-24 14:38:00 +0000

    Create NewsArticle.java

---
+++ app/src/main/java/com/Freddy/infomix/models/NewsArticle.java
@@ -0,0 +1,10 @@
+package com.yourname.infomix.models;
+
+public class NewsArticle {
+    public String title;
+    public String description;
+    public String url;
+    public String urlToImage;
+    public String publishedAt;
+    public String source;
+}
